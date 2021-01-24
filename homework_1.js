035ef93c45407b3b24e6c9a8419ad6d49fee1123
Ross <73292006+RossKostenko@users.noreply.github.com>
2021-01-24 20:21:23 +0200

    Create homework_1.js

---
+++ homework_1.js
@@ -0,0 +1,62 @@
+// task 3
+let firstVariable;
+let secondVariable; 
+firstVariable = 1000;
+secondVariable = 2000;
+secondVariable = firstVariable;
+return firstVariable;
+return secondVariable;
+
+// task 4
+let testObject = {
+	String = "String",
+	Number = 5,
+	Boolean = true,
+	Undefined = undefined,
+	Null = null
+};
+
+// task 5
+let isAdult = confirm("Are you adult?");
+console.log(isAdult);
+
+// task 6
+const yourName;
+const yourSurname;
+const learningGroup;
+const yearOfBirth;
+
+yourName = "Rostyslav";
+yourSurname = "Kostenko";
+learningGroup = 569;
+yearOfBirth = 1986;
+
+const isMaried = true; 
+
+typeof yourName;
+typeof yourSurname;
+typeof learningGroup;
+typeof yearOfBirth;
+typeof isMaried;
+
+console.log(yourName);
+console.log(yourSurname);
+console.log(earningGroup);
+console.log(yearOfBirth);
+console.log(isMaried);
+
+// task 7
+let userName = promt("Please enter your name","User");
+let userMail = promt("Please enter your e-mail","usermale@gmail.com");
+let userPassword = promt("Please enter your password","qwerty");
+return "Dear "+ userName + ", your email is " + userMail + ", your password is " + userPassword;
+
+//task 8
+const secondsInHour = 3600;
+const secondsInDay = 24*secondsInHour;
+const secondsInMonth = 30*secondsInDay;
+
+return secondsInHour;
+return secondsInDay; 
+return secondsInMonth;
+
