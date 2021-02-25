ef9e191cf3432115645fe04e8d28b7ccdd1a207c
Ross <73292006+RossKostenko@users.noreply.github.com>
2021-02-25 13:43:23 +0200

    Create Hometask_9_JQUERY.js

---
+++ Hometask_9_JQUERY.js
@@ -0,0 +1,19 @@
+// Task 1
+
+$("a[href^='https://']").attr('target', '_blank');
+
+// Task 2
+
+let $elem = $(".head")
+
+$elem.css("backgroundColor", "green");
+
+$elem.find(".inner").css("fontsize", "35px");
+
+// Task 3
+
+$("h3").foreach(function(i, le) {
+	let $elem = $("h3").next();
+	$("h3").next().remove();
+	$("h3").prepend($elem);
+})
