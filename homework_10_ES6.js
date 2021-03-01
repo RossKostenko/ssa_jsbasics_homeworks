c002031d2c8cc8ab1b3d9b93e5c19d795e13da30
Ross <73292006+RossKostenko@users.noreply.github.com>
2021-03-01 15:30:29 +0200

    Update homework_10_ES6.js

--- homework_10_ES6.js
+++ homework_10_ES6.js
@@ -5,3 +5,35 @@ let [x, y, , ...z] = arr;
 console.log(x); // "Tom"
 console.log(y); // "Sam"
 console.log(z); // [Bob]
+
+
+// Task 2
+
+let data = {
+   names: ["Sam", "Tom", "Ray", "Bob"],
+   ages: [20, 24, 22, 26],
+};
+let [,name2,,name4] = data.names;
+let [,age2,,age4] = data.ages;
+console.log(name2); // "Tom"
+console.log(age2); // 24
+console.log(name4); // "Bob"
+console.log(age4); // 26
+
+// Task 3
+
+function mul(...rest) {
+  if (rest.some((x) => typeof x === "number")) {
+    let args = [];
+    for (let value of rest) {
+      if (typeof value === "number") {
+        args.push(value);
+      }
+    }
+    return args.reduce((prev, next) => prev * next);
+  } else {
+    return 0;
+  }
+}
+console.log(mul(1, "str", 2, 3, true)); // 6
+console.log(mul(null, "str", false, true)); // 0
